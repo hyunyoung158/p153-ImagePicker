aa575a0c46f788bf708efe25b66eb52d7c91d7df
hyunyoung158 <setarong158@gmail.com>
2014-01-06 16:00:12 +0900

    ImagePicker 실습프로젝트입니다.

---
+++ ImagePicker/ViewController.m
@@ -0,0 +1,63 @@
+//
+//  ViewController.m
+//  ImagePicker
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+#define MAX_NUM 4
+@interface ViewController ()<UIPickerViewDelegate, UIPickerViewDataSource>
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
+
+@end
+
+@implementation ViewController
+
+- (IBAction)selectRandom:(id)sender {
+    int r = arc4random() % MAX_NUM;
+    [self.picker selectRow:r inComponent:0 animated:YES];
+}
+
+//컴포넌트 개수
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return MAX_NUM;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+    return 64;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+    NSString *imagePath = [NSString stringWithFormat:@"girl%d.jpg", (int)row];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    UIImageView *imageView;
+    
+    if (nil == view) {
+        imageView = [[UIImageView alloc]initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+    }else {
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+    }
+    return imageView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
